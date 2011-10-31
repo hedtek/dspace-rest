f6dd7dcee2bac1338dad7fd4d8253aeb0f625543
Dave <workmad3@gmail.com>
2011-10-31 17:28:47 +0000

    Refactored matcher class to tidy up array matching

--- src/test/java/uk/ac/jorum/integration/matchers/ContainsJSONKey.java
+++ src/test/java/uk/ac/jorum/integration/matchers/ContainsJSONKey.java
@@ -6,13 +6,27 @@ import org.hamcrest.Matcher;
 import org.json.simple.JSONObject;
 import org.junit.internal.matchers.TypeSafeMatcher;
 
-public class ContainsJSONKey <T> extends TypeSafeMatcher<JSONObject> {
+public class ContainsJSONKey <T> {
+	
+	@Factory
+	public static Matcher<JSONObject> hasKey( String key ) {
+	    return new ContainsJSONKeyValue<Object>(key);
+	}
 
-	private enum Mode { ONLY_KEY, VALUE, VALUES }
+	@Factory
+	public static <T> Matcher<JSONObject> hasKey( String key, T value) {
+	    return new ContainsJSONKeyValue<T>(key, value);
+	}
+	
+	@Factory
+	public static <T> Matcher<JSONObject> hasKey( String key, T[] values) {
+	    return new ContainsJSONKeyArray<T>(key, values);
+	}
+}
+
+class ContainsJSONKeyValue <T> extends TypeSafeMatcher<JSONObject> {
 	private String key;
 	private T expectedValue;
-	private T[] expectedValues;
-	private Mode checkMode;
 	private boolean isKeyPresent;
 	
 	@Override
@@ -28,28 +42,53 @@ public class ContainsJSONKey <T> extends TypeSafeMatcher<JSONObject> {
 	public boolean matchesSafely(JSONObject item) {
 		isKeyPresent = item.containsKey(key);
 		if(!isKeyPresent) {
-			return isKeyPresent;
+			return false;
 		}
-		
-		T actualValue = (T)item.get(key);
+		if (expectedValue == null) {
+			return true;
+		}
 
-		if(checkMode == Mode.VALUE) {
-			return compareWithExpectedValue(actualValue);
-		} else if (checkMode == Mode.VALUES) {
-			return findInExpectedValues(actualValue);
-		}
+		T actualValue = (T)item.get(key);
 		
-		return isKeyPresent;
-	}
-	
-	private boolean compareWithExpectedValue(T actualValue) {
 		if (actualValue == null) {
 			return (expectedValue == null);
 		} 
 		return actualValue.equals(expectedValue);
 	}
+
+	ContainsJSONKeyValue(String key) {
+        this.key = key;
+    }
 	
-	private boolean findInExpectedValues(T actualValue) {
+	ContainsJSONKeyValue(String key, T value) {
+        this.key = key;
+        this.expectedValue = value;
+    }
+}
+
+class ContainsJSONKeyArray <T> extends TypeSafeMatcher<JSONObject> {
+	private String key;
+	private T[] expectedValues;
+	private boolean isKeyPresent;
+	
+	@Override
+	public void describeTo(Description description) {
+		if(!isKeyPresent) {
+			description.appendText("Key not present: " + key);
+		} else {
+			description.appendText("Value of " + key + " to be " + expectedValues);
+		}
+	}
+
+	@Override
+	public boolean matchesSafely(JSONObject item) {
+		isKeyPresent = item.containsKey(key);
+		if(!isKeyPresent) {
+			return false;
+		}
+
+		T actualValue = (T)item.get(key);
+		
 		if (actualValue == null) {
 			for (T expected: expectedValues) {
 				if(expected==null) {
@@ -65,36 +104,10 @@ public class ContainsJSONKey <T> extends TypeSafeMatcher<JSONObject> {
 		}
 		return false;
 	}
-
-	private ContainsJSONKey(String key) {
-        this.key = key;
-        checkMode = Mode.ONLY_KEY;
-    }
 	
-	private ContainsJSONKey(String key, T value) {
-        this.key = key;
-        this.expectedValue = value;
-        checkMode = Mode.VALUE;
-    }
-	
-	private ContainsJSONKey(String key, T[] values) {
+	ContainsJSONKeyArray(String key, T[] values) {
         this.key = key;
         this.expectedValues = values;
-        checkMode= Mode.VALUES;
     }
-	
-	@Factory
-	public static Matcher<JSONObject> hasKey( String key ) {
-	    return new ContainsJSONKey<Object>(key);
-	}
-
-	@Factory
-	public static <T> Matcher<JSONObject> hasKey( String key, T value) {
-	    return new ContainsJSONKey<T>(key, value);
-	}
-	
-	@Factory
-	public static <T> Matcher<JSONObject> hasKey( String key, T[] values) {
-	    return new ContainsJSONKey<T>(key, values);
-	}
 }
+
