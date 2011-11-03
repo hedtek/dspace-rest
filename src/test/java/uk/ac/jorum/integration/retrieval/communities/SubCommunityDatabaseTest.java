41fd696af7aa4f7a08461c123da5bd182b6f3f9c
Ashish <asehra@gmail.com>
2011-11-03 11:11:55 +0000

    Refactored SubCommunityDatabase to use the structural matchers

--- src/test/java/uk/ac/jorum/integration/retrieval/communities/SubCommunityDatabaseTest.java
+++ src/test/java/uk/ac/jorum/integration/retrieval/communities/SubCommunityDatabaseTest.java
@@ -1,9 +1,10 @@
 package uk.ac.jorum.integration.retrieval.communities;
 
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static uk.ac.jorum.integration.matchers.EntityMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+
 
 import java.util.ArrayList;
 
@@ -15,9 +16,28 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import uk.ac.jorum.integration.RestApiBaseTest;
-import uk.ac.jorum.integration.matchers.ContainsJSONKey;
 
 public class SubCommunityDatabaseTest extends RestApiBaseTest {
+	
+	private ArrayList<Matcher<JSONObject>> subCommunityListMatchers = new ArrayList<Matcher<JSONObject>>() {
+		{
+			add(isCommunityId(4));
+		}
+	};
+
+	private final Matcher<JSONObject> parentCommunity = isCommunity(2,
+			"Community no 1", "123456789/2",
+			"Introductory text for community no 1",
+			"Short description of community no 1",
+			"Side bar text for community 1", "Copyright information", 0,
+			null, subCommunityListMatchers, emptyMatcherList(),
+			emptyMatcherList());
+
+	private final Matcher<JSONObject> community = isCommunity(4, "Sub-community", "123456789/4",
+			"Introductory text for the sub-community",
+			"This is a sub-community for a top-level community",
+			"",	"", 0, parentCommunity, emptyMatcherList(),
+			emptyMatcherList(), emptyMatcherList());
 	
 	@BeforeClass
     public static void createFixture() throws Exception {
@@ -45,49 +65,19 @@ public class SubCommunityDatabaseTest extends RestApiBaseTest {
 	public void subCommunityInListShouldHaveParentInformation() throws Exception {
 	  String result = makeRequest("/communities", "topLevelOnly=false");
 	  JSONObject resultJSON = (JSONObject) JSONValue.parse(result);
-	  
-	  
-	  ArrayList<Matcher<JSONObject>> subCommunityListMatchers = new ArrayList<Matcher<JSONObject>>(){{
-		  add(isCommunityId(4));
-	  }};
-
-	  final Matcher<JSONObject> parentCommunityMatcher = isCommunity(2, "Community no 1", "123456789/2",
-				"Introductory text for community no 1",
-				"Short description of community no 1",
-				"Side bar text for community 1",
-				"Copyright information", 0, null, subCommunityListMatchers,
-				emptyMatcherList(), emptyMatcherList());
-	  final Matcher<JSONObject> communityMatcher = isCommunity(4, "Sub-community", "123456789/4",
-				"Introductory text for the sub-community",
-				"This is a sub-community for a top-level community",
-				"",	"", 0, parentCommunityMatcher, emptyMatcherList(),
-				emptyMatcherList(), emptyMatcherList());
-	  
+	  	  
 	  ArrayList<Matcher<JSONObject>> communityListMatchers = new ArrayList<Matcher<JSONObject>>() {{
-		add(parentCommunityMatcher);
-		add(communityMatcher);
+		add(parentCommunity);
+		add(community);
 	  }};
 	  
 	  assertThat(resultJSON, hasArray("communities_collection", communityListMatchers));
 	}
 	  
 	@Test
-	public void parentLevelCommunityShouldHaveSubCommunityInformation() throws Exception {
-	  String result = makeRequest("/communities", "topLevelOnly=false");
-	  JSONObject resultJSON = (JSONObject) JSONValue.parse(result);
-	  JSONArray communityList = (JSONArray) resultJSON.get("communities_collection");
-	  
-	  JSONObject community = (JSONObject) communityList.get(0);
-	  JSONArray subCommunitiesList = (JSONArray) community.get("subCommunities");
-	  JSONObject subCommunity = (JSONObject) subCommunitiesList.get(0);
-	  assertThat(subCommunity, hasId(4));
-	}
-	
-	@Test
 	public void showCommunityAnchestorShouldHaveDataWithParentDetailsForSubcommunities() throws Exception {
 		String result = makeRequest("/communities/4/anchestor");
-		JSONObject community = (JSONObject) JSONValue.parse(result);
-		
-		assertThat(community, hasId(2));
+		JSONObject anchestor = (JSONObject) JSONValue.parse(result);
+		assertThat(anchestor, is(parentCommunity));
 	}  
 }
