50348359aa32c53ee769f4be76ca7624bb26969f
Dave <workmad3@gmail.com>
2011-11-10 10:17:44 +0000

    Added search index to test resources. Paramterised the resources directory to allow different fixtures for jorum and later dspace versions

--- make_fixture.sh
+++ make_fixture.sh
@@ -2,5 +2,5 @@
 
 pg_dump dspace --insert --use-set-session-authorization > tmp_fixture
 
-sed -e '/^SET/ d' -e '/^REVOKE/ d' -e '/^GRANT/ d' -e "s/^\(INSERT INTO bitstream VALUES.*'\).*\(\/upload.*$\)/\1\$\{resource.test.base\}\2/" tmp_fixture > src/test/resources/fixtures/$1.sql
+sed -e '/^SET/ d' -e '/^REVOKE/ d' -e '/^GRANT/ d' -e "s/^\(INSERT INTO bitstream VALUES.*'\).*\(\/upload.*$\)/\1\$\{resource.test.base\}\2/" tmp_fixture > src/test/resources-1.5.2/fixtures/$1.sql
 rm tmp_fixture
