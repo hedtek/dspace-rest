b625bc4d2132c10cd963543bd452dee1e9f203af
Ashish Sehra <ashish@hedtek.com>
2012-02-09 15:11:14 +0000

    Improved quality

--- src/main/java/org/dspace/rest/providers/Binder.java
+++ src/main/java/org/dspace/rest/providers/Binder.java
@@ -20,10 +20,6 @@ import org.sakaiproject.entitybus.exception.EntityException;
 public class Binder {
     public static Binder forUsers() throws SecurityException, NoSuchMethodException {
         Map<String, String> func2actionMapGET = new HashMap<String, String>();
-        Map<String, Class<?>[]> funcParamsGET = new HashMap<String, Class<?>[]>();
-        Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
-        
-        Class processedEntity = UserEntity.class;
         func2actionMapGET.put("getEmail", "email");
         func2actionMapGET.put("getFirstName", "firstName");
         func2actionMapGET.put("getFullName", "fullName");
@@ -36,17 +32,17 @@ public class Binder {
         func2actionMapGET.put("getRequireCertificate", "requireCertificate");
         func2actionMapGET.put("getSelfRegistered", "selfRegistered");
         func2actionMapGET.put("getType", "type");
-        Constructor<?>  entityConstructor = processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
-        initMappings(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, processedEntity);
-        return new Binder(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, entityConstructor);
+        return build(func2actionMapGET, UserEntity.class);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static Binder build(Map<String, String> func2actionMapGET,
+            Class processedEntity) throws NoSuchMethodException {
+        return new Binder(mappings(func2actionMapGET, processedEntity), constructor(processedEntity));
     }
     
     public static Binder forCommunities() throws SecurityException, NoSuchMethodException {
         Map<String, String> func2actionMapGET = new HashMap<String, String>();
-        Map<String, Class<?>[]> funcParamsGET = new HashMap<String, Class<?>[]>();
-        Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
-        
-        Class processedEntity = CommunityEntity.class;
         func2actionMapGET.put("getId", "id");
         func2actionMapGET.put("getName", "name");
         func2actionMapGET.put("getCountItems", "countItems");
@@ -62,17 +58,11 @@ public class Binder {
         func2actionMapGET.put("getSidebarText", "sidebarText");
         func2actionMapGET.put("getIntroductoryText", "introductoryText");
         func2actionMapGET.put("getLogo", "logo");
-        Constructor<?>  entityConstructor = processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
-        initMappings(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, processedEntity);
-        return new Binder(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, entityConstructor);
+        return build(func2actionMapGET, CommunityEntity.class);
     }
     
     public static Binder forCollections() throws SecurityException, NoSuchMethodException {
-        Class processedEntity = CollectionEntity.class;
-
         Map<String, String> func2actionMapGET = new HashMap<String, String>();
-        Map<String, Class<?>[]> funcParamsGET = new HashMap<String, Class<?>[]>();
-        Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
         func2actionMapGET.put("getId", "id");
         func2actionMapGET.put("getName", "name");
         func2actionMapGET.put("getLicence", "licence");
@@ -88,16 +78,11 @@ public class Binder {
         func2actionMapGET.put("getSidebarText", "sidebarText");
         func2actionMapGET.put("getProvenance", "provenance");
         func2actionMapGET.put("getLogo", "logo");
-        Constructor<?>  entityConstructor = processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
-        initMappings(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, processedEntity);
-        return new Binder(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, entityConstructor);
+        return build(func2actionMapGET, CollectionEntity.class);
     }
     
     public static Binder forBitstream() throws SecurityException, NoSuchMethodException {
-        Class processedEntity = BitstreamEntity.class;
         Map<String, String> func2actionMapGET = new HashMap<String, String>();
-        Map<String, Class<?>[]> funcParamsGET = new HashMap<String, Class<?>[]>();
-        Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
         func2actionMapGET.put("getMimeType", "mimeType");
         func2actionMapGET.put("getBundles", "bundles");
         func2actionMapGET.put("getCheckSum", "checkSum");
@@ -113,16 +98,11 @@ public class Binder {
         func2actionMapGET.put("getHandle", "handle");
         func2actionMapGET.put("getId", "id");
         func2actionMapGET.put("getType", "type");
-        Constructor<?>  entityConstructor = processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
-        initMappings(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, processedEntity);
-        return new Binder(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, entityConstructor);
+        return build(func2actionMapGET, BitstreamEntity.class);
     }
     
     public static Binder forItem() throws SecurityException, NoSuchMethodException {
-        Class processedEntity = ItemEntity.class;
         Map<String, String> func2actionMapGET = new HashMap<String, String>();
-        Map<String, Class<?>[]> funcParamsGET = new HashMap<String, Class<?>[]>();
-        Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
         func2actionMapGET.put("getMetadata", "metadata");
         func2actionMapGET.put("getSubmitter", "submitter");
         func2actionMapGET.put("getIsArchived", "isArchived");
@@ -138,17 +118,12 @@ public class Binder {
         func2actionMapGET.put("getId", "id");
         func2actionMapGET.put("getType", "type");
         func2actionMapGET.put("getBundles", "bundles");
-        Constructor<?>  entityConstructor = processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
-        initMappings(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, processedEntity);
-        return new Binder(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, entityConstructor);
+        return build(func2actionMapGET, ItemEntity.class);
     }
 
     
     public static Binder forGroup() throws SecurityException, NoSuchMethodException {
-        Class processedEntity = GroupEntity.class;
         Map<String, String> func2actionMapGET = new HashMap<String, String>();
-        Map<String, Class<?>[]> funcParamsGET = new HashMap<String, Class<?>[]>();
-        Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
         func2actionMapGET.put("getEmail", "email");
         func2actionMapGET.put("getFirstName", "firstName");
         func2actionMapGET.put("getFullName", "fullName");
@@ -161,86 +136,55 @@ public class Binder {
         func2actionMapGET.put("getRequireCertificate", "requireCertificate");
         func2actionMapGET.put("getSelfRegistered", "selfRegistered");
         func2actionMapGET.put("getType", "type");
-        Constructor<?>  entityConstructor = processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
-        initMappings(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, processedEntity);
-        return new Binder(func2actionMapGET, funcParamsGET, func2actionMapGET_rev, entityConstructor);
+        return build(func2actionMapGET, GroupEntity.class);
     }
 
-    private final Map<String, String> func2actionMapGET;
-    private final Map<String, Class<?>[]> funcParamsGET;
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static Constructor<?> constructor(Class processedEntity)
+            throws NoSuchMethodException {
+        return (Constructor<?>) processedEntity.getDeclaredConstructor(new Class<?>[]{String.class, Context.class, Integer.TYPE, DetailDepth.class});
+    }
+
+    private static Map<String, String> mappings(Map<String, String> func2actionMapGET,
+            Class<?> processedEntity) throws NoSuchMethodException {
+        final Map<String, String> func2actionMapGET_rev = new HashMap<String, String>();
+        final Method[] entityMethods = processedEntity.getMethods();
+        for (Method m : entityMethods) {
+            final String fieldGET = func2actionMapGET.get(m.getName());
+            if (fieldGET != null) {
+                func2actionMapGET_rev.put(fieldGET, m.getName());
+            }
+        }
+        return func2actionMapGET_rev;
+    }
+    
     private final Map<String, String> func2actionMapGET_rev ;
     private final Constructor<?> entityConstructor;
 
-    private Binder(Map<String, String> func2actionMapGET,
-            Map<String, Class<?>[]> funcParamsGET,
-            Map<String, String> func2actionMapGET_rev,
+    private Binder(Map<String, String> func2actionMapGET_rev,
             Constructor<?> entityConstructor) {
         super();
-        this.func2actionMapGET = func2actionMapGET;
-        this.funcParamsGET = funcParamsGET;
         this.func2actionMapGET_rev = func2actionMapGET_rev;
         this.entityConstructor = entityConstructor;
     }
-
-
-    private static void initMappings(Map<String, String> func2actionMapGET,
-            Map<String, Class<?>[]> funcParamsGET,
-            Map<String, String> func2actionMapGET_rev,
-            Class<?> processedEntity) throws NoSuchMethodException {
-        // scan for methods;
-        Method[] entityMethods = processedEntity.getMethods();
-        for (Method m : entityMethods) {
-            //log.info("checked method " + m.getName());
-            String fieldGET = func2actionMapGET.get(m.getName());
-            if (fieldGET != null) {
-                addParameters(fieldGET, m.getParameterTypes(), funcParamsGET);
-                addMethod(fieldGET, m.getName(), func2actionMapGET_rev);
-            }
-        }
-    }
-    
-
-    private static void addParameters(String function, Class<?>[] parameters, Map<String, Class<?>[]> mappings_parameters) {
-        mappings_parameters.put(function, parameters);
-    }
-
-    private Class<?>[] getParameters(String function, Map<String, Class<?>[]> mappings_parameters) {
-        return mappings_parameters.get(function);
-    }
-
-    private static void addMethod(String field, String function, Map<String, String> mappings_rev) {
-        mappings_rev.put(field, function);
-    }
-
-    private String getMethod(String field, Map<String, String> mappings_rev) {
-        return mappings_rev.get(field);
-    }
-    
 
     public Object resolve(EntityReference reference, String[] segments,
             DetailDepthParameters depth, final Context context) {
         if (func2actionMapGET_rev.containsKey(segments[3])) {
             Object result;
-            String function = getMethod(segments[3], func2actionMapGET_rev);
-            
-
+            String function = func2actionMapGET_rev.get(segments[3]);
             Object CE = new Object();
             try {
-
                 CE = entityConstructor.newInstance(reference.getId(), context, 1, depth.getDepth());
             } catch (Exception ex) {
                 throw new EntityException("Internal server error", "Cannot create entity", 500);
             }
-
             try {
                 Method method = CE.getClass().getMethod(function, new Class<?>[]{});
-                //Method method = CE.getClass().getMethod(function, new Class<?>[]{EntityView.class, Map.class, Context.class});
-                result = method.invoke(CE); // TODO more flexible for other param types
+                result = method.invoke(CE); 
             } catch (Exception ex) {
                 throw new EntityException("Internal server error", "Cannot call method " + function, 500);
             }
-
-           
             return result;
         } else {
             throw new EntityException("Bad request", "Method not supported " + segments[3], 400);
