f5a94ce0169da35e737c8a0a3453b89fe71ef749
Ashish Sehra <ashish@hedtek.com>
2012-02-13 17:41:07 +0000

    Simplified code by eliminating unnecessary inheritance.

--- src/main/java/org/dspace/rest/providers/Binder.java
+++ src/main/java/org/dspace/rest/providers/Binder.java
@@ -4,21 +4,25 @@ import java.sql.SQLException;
 
 import org.dspace.core.Context;
 import org.dspace.rest.diagnose.Operation;
+import org.dspace.rest.diagnose.SQLFailureEntityException;
 import org.dspace.rest.entities.BitstreamEntity;
 import org.dspace.rest.entities.CollectionEntity;
+import org.dspace.rest.entities.CommunityEntity;
+import org.dspace.rest.entities.DetailDepth;
 import org.dspace.rest.entities.GroupEntity;
 import org.dspace.rest.entities.ItemEntity;
 import org.dspace.rest.entities.UserEntity;
 import org.dspace.rest.params.Parameters;
 import org.dspace.rest.params.Route;
+import org.sakaiproject.entitybus.exception.EntityException;
 
-public class Binder {
+public abstract class Binder {
     
     public static Binder forUsers() throws SecurityException, NoSuchMethodException {
-        return new Binder(new UserAttributeValuer());
+        return new UserBinder();
     }
     
-    private static class UserAttributeValuer extends DirectAttributeValuer {
+    private static class UserBinder extends Binder{
 
         @Override
         protected Object value(String id, Parameters parameters,
@@ -46,14 +50,58 @@ public class Binder {
     }
     
     public static Binder forCommunities() throws SecurityException, NoSuchMethodException {
-        return new Binder(new CommunitiesAttributeValuer());
+        return new CommunitiesBinder();
     }
     
+    private static class CommunitiesBinder extends Binder {
+        @Override
+        protected Object value(String id, Parameters parameters, Context context,
+                String attributeSegment) throws SQLException {
+            final DetailDepth depth = parameters.getDetailDepth().getDepth();
+            if ("id".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getId();
+            } else if ("name".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getName();
+            } else if ("countItems".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getCountItems();
+            } else if ("handle".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getHandle();
+            } else if ("type".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getType();
+            } else if ("collections".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getCollections();
+            } else if ("canedit".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getCanEdit();
+            } else if ("anchestor".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getParentCommunity();
+            } else if ("children".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getSubCommunities();
+            } else if ("recent".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getRecentSubmissions();
+            } else if ("shortDescription".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getShortDescription();
+            } else if ("copyrightText".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getCopyrightText();
+            } else if ("sidebarText".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getSidebarText();
+            } else if ("introductoryText".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getIntroductoryText();
+            } else if ("logo".equals(attributeSegment)) {
+                return new CommunityEntity(id, context, 1, depth).getLogo();
+            } else {
+                return null;
+            }
+        }
+     
+        protected Operation operation() {
+            return Operation.GET_COMMUNITIES;
+        }
+    }
     public static Binder forCollections() throws SecurityException, NoSuchMethodException {
-        return new Binder(new CollectionAttributeValuer());
+        return new CollectionBinder();
     }
     
-    private static class CollectionAttributeValuer extends DirectAttributeValuer {
+    private static class CollectionBinder extends Binder{
 
         @Override
         protected Object value(String id, Parameters parameters,
@@ -102,10 +150,10 @@ public class Binder {
         
     }
     public static Binder forBitstream() throws SecurityException, NoSuchMethodException {
-        return new Binder(new BitstreamAttributeValuer());
+        return new BitstreamBinder();
     }
     
-    private static class BitstreamAttributeValuer extends DirectAttributeValuer {
+    private static class BitstreamBinder extends Binder{
 
         @Override
         protected Object value(String id, Parameters parameters,
@@ -153,10 +201,10 @@ public class Binder {
     }
     
     public static Binder forItem() throws SecurityException, NoSuchMethodException {
-        return new Binder(new ItemAttributeValuer());
+        return new ItemBinder();
     }
     
-    private static class ItemAttributeValuer extends DirectAttributeValuer {
+    private static class ItemBinder extends Binder{
 
         @Override
         protected Object value(String id, Parameters parameters,
@@ -205,10 +253,10 @@ public class Binder {
 
     
     public static Binder forGroup() throws SecurityException, NoSuchMethodException {
-        return new Binder(new GroupAttributeValuer());
+        return new GroupBinder();
     }
     
-    private static class GroupAttributeValuer extends DirectAttributeValuer {
+    private static class GroupBinder extends Binder{
 
         @Override
         protected Object value(String id, Parameters parameters,
@@ -240,16 +288,31 @@ public class Binder {
         
     }
     
-    
-    private final AttributeValuer valuer;
 
-    private Binder(final AttributeValuer valuer) {
+    protected Binder() {
         super();
-        this.valuer = valuer;
     }
 
     public Object resolve(final String id, Route route, Parameters parameters,
             final Context context) {
-        return valuer.valueAttribute(id, parameters, context, route.attributeSegment());
+        return valueAttribute(id, parameters, context, route.attributeSegment());
     }
+
+    protected abstract Object value(String id, Parameters parameters,
+            Context context, String attributeSegment) throws SQLException;
+    
+    public Object valueAttribute(String id, Parameters parameters, Context context,
+            String attributeSegment) {
+                try {
+                    final Object value = value(id, parameters, context, attributeSegment);
+                    if (value == null) {
+                        throw new EntityException("Bad request", "Method not supported " + attributeSegment, 404);
+                    }
+                    return value;
+                } catch (SQLException cause) {
+                    throw new SQLFailureEntityException(operation(), cause);
+                }
+            }
+
+    protected abstract Operation operation();
 }
